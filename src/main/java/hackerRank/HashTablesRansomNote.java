21050d2d51621d5b65d8eb698fa1de962d246642
azsena1997 <azsena2020>
2020-06-22 16:54:59 +0400

    --- HashTablesRansomNote Java 8  added.

---
+++ src/main/java/hackerRank/HashTablesRansomNote.java
@@ -0,0 +1,24 @@
+package hackerRank;
+
+import java.util.Arrays;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class HashTablesRansomNote {
+    public static void main(String[] args) {
+
+        String[] magazine = {"give", "me", "one", "grand", "today", "night"};
+        String[] note = {"give", "one", "grand", "today"};
+        checkMagazine(magazine, note);
+    }
+
+    // Complete the checkMagazine function below.
+    static void checkMagazine(String[] magazine, String[] note) {
+        Map<String, Long> words = Arrays.stream(magazine).collect(Collectors.groupingBy(w -> w, Collectors.counting()));
+/**   merge -> get ,  operation  , put **/
+        boolean ok = Arrays.stream(note).allMatch(w -> words.merge(w, -1L, Long::sum) >= 0);
+
+        System.out.println(ok ? "Yes" : "No");
+    }
+
+}
