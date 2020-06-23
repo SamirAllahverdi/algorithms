62fd24d92b977949ca4233b5d3d1a9db65d9a4d1
azsena1997 <azsena2020>
2020-06-23 14:17:56 +0400

    --- The new approach which is amazing(Bit operators) added. (InvertString)

---
+++ src/main/java/InterestingWarmUpAlgorithms/InvertString.java
@@ -0,0 +1,50 @@
+package InterestingWarmUpAlgorithms;
+
+public class InvertString {
+    // contains only [a-zA-Z]
+    // abc -> ABC
+    // aBc -> AbC
+    // Abc -> aBC
+    // 1.
+
+    // 2. NO if. NO tern op.
+    String invert(String origin) {
+        byte[] bytes = origin.getBytes();
+        for (int i = 0; i < bytes.length; i++) {
+            int bit = 0b100000;
+            bytes[i] ^=32;
+//            chars[i] = (char) (chars[i] ^ bit); // switch vice versa
+        }
+        return new String(bytes);
+    }
+
+    String toUpperCase(String origin) { // a -> A A -> A
+        char[] chars = origin.toCharArray();
+        for (int i = 0; i < chars.length; i++) {
+            int bit = 0b00100000;
+            int mask = ~bit; //0b11011111
+//            &  = multiplication
+            chars[i] = (char) (chars[i] & mask); // switch OFF bit &&
+        }
+        return new String(chars);
+    }
+
+    String toLowerCase(String origin) { // A -> a a -> a
+        char[] chars = origin.toCharArray();
+        for (int i = 0; i < chars.length; i++) {
+            int bit = 0b100000;
+//            | =  increment
+            chars[i] = (char) (chars[i] | bit); // switch ON bit
+        }
+        return new String(chars);
+    }
+    // ~, &, |, <<, >>
+
+    public static void main(String[] args) {
+        InvertString is = new InvertString();
+        System.out.println(is.invert("abCeee"));
+        System.out.println(is.toLowerCase("abC"));
+        System.out.println(is.toUpperCase("abC"));
+
+    }
+}
