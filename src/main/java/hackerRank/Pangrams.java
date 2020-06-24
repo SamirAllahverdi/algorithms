0e91996e3f9b63dc72e6fdf291122502e742fa39
azsena1997 <azsena2020>
2020-06-24 15:26:17 +0400

    --- Pangrams added.

---
+++ src/main/java/hackerRank/Pangrams.java
@@ -0,0 +1,36 @@
+package hackerRank;
+
+import javax.naming.InsufficientResourcesException;
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
+public class Pangrams {
+    public static void main(String[] args) {
+//        System.out.println(pangrams("We promptly judged antique ivory buckles for the prize"));
+        System.out.println(pangrams("We promptly judged antique ivory buckles for the next prize"));
+        System.out.println(pangrams2("We promptly judged antique ivory buckles for the next prize"));
+
+    }
+
+    static String pangrams2(String s) {
+        return IntStream.range(0, 26).allMatch(a -> s.toLowerCase().contains(String.valueOf((char) (a + 97)))) ?
+                "pangram" : "not pangram";
+    }
+
+    static String pangrams(String s) {
+        int[] alphabet = new int[26];
+
+
+        s.codePoints().forEach(a -> {
+            if (Character.isUpperCase(a)) {
+                alphabet[a - 65]++;
+            } else if (Character.isLowerCase(a)) {
+                alphabet[a - 97]++;
+            }
+        });
+
+        return Arrays.stream(alphabet).anyMatch(a -> a == 0) ? "not pangram" : "pangram";
+    }
+
+
+}
