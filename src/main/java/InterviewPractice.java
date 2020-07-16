a638c5616abc6467303931985766a90213715428
Samir <a_nazim2007@mail.ru>
2020-07-16 19:27:32 +0400

    ---  FunnyString added.

--- src/main/java/InterviewPractice.java
+++ src/main/java/InterviewPractice.java
@@ -1,6 +1,24 @@
+import java.util.Arrays;
+import java.util.List;
+
 public class InterviewPractice {
     public static void main(String[] args) {
         Runtime runtime = Runtime.getRuntime();
         runtime.gc();
+
+        int [] array ={10, 7, 2, 8, 3};
+
+
     }
+
+
+
+    private static boolean isPower(int c) {
+        System.out.println("IS POWER " + c);
+        if( c == 2) return true;
+        else if(c % 2 == 1 || c== 0) return false;
+        return isPower(c/2);
+    }
+
+
 }
---
+++ src/main/java/hackerRank/easy/FunnyString.java
@@ -0,0 +1,29 @@
+package hackerRank.easy;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class FunnyString {
+
+    public static void main(String[] args) {
+        String a = "acxz";
+        String b ="bcxz";
+        System.out.println(funnyStringWithStream(b));
+        System.out.println(funnyStringWithStreamOptimized(b));
+    }
+
+
+    static String funnyStringWithStream(String s) {
+
+        List<Integer> order = IntStream.range(0, s.length() - 1).boxed().map(a -> Math.abs(s.charAt(a) - s.charAt(a + 1))).collect(Collectors.toList());
+        List<Integer> reverseOrder = IntStream.range(1, s.length()).boxed().sorted(Comparator.reverseOrder()).map(a -> Math.abs(s.charAt(a) - s.charAt(a - 1))).collect(Collectors.toList());
+
+        return order.equals(reverseOrder) ? "Funny" : "Not Funny";
+    }
+
+    static String funnyStringWithStreamOptimized(String s) {
+        return IntStream.range(0, s.length() - 1).allMatch(i-> Math.abs(s.charAt(i) - s.charAt(i+1)) == Math.abs(s.charAt(s.length()-i-1) - s.charAt(s.length()-i-2))) ? "Funny" : "Not Funny";
+    }
+}
