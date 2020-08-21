d7392294bef1dc751ec8dca8ac4fb681a96a72fc
SamirAllahverdi <a_nazim2007@mail.ru>
2020-08-21 16:30:03 +0400

    ---  RemoveDuplicatesFromSortedArray added.

--- src/main/java/interview/InterviewPractice.java
+++ src/main/java/interview/InterviewPractice.java
@@ -3,6 +3,21 @@ package interview;
 public class InterviewPractice {
 
     public static void main(String[] args) {
+//   Reverse String
+        String name = "Samir";
+
+        System.out.println(reverse(name));
 
     }
+
+    private static String reverse(String name) {
+        for (int i = 0; i < name.length(); i++) {
+            name = String.format("%s%s%s",name.substring(1, name.length() - i),
+                    name.substring(0, 1),
+                    name.substring(name.length() - i));
+        }
+        return name;
+    }
+
+
 }
---
+++ src/main/java/leetcode/easy/RemoveDuplicatesFromSortedArray.java
@@ -0,0 +1,33 @@
+package leetcode.easy;
+
+import java.io.OutputStream;
+import java.util.Arrays;
+
+public class RemoveDuplicatesFromSortedArray {
+
+    /**
+     * https://leetcode.com/problems/remove-duplicates-from-sorted-array/
+     */
+
+    public static void main(String[] args) {
+        int[] nums = {0,0,1,1,1,2,2,3,3,4};
+
+        System.out.println(removeDuplicates(nums));
+
+    }
+
+    public static int removeDuplicates(int[] nums) {
+        if(nums.length == 0) return 0;
+
+        int i = 0;
+        for (int j = 1; j < nums.length; j++) {
+            if (nums[j] != nums[i]) {
+                i++;
+                nums[i] = nums[j];
+            }
+
+        }
+        return i + 1;
+    }
+
+}
