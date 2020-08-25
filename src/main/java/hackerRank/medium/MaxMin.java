7e5c74b2ffd886eaf3d3ef9fad76f28fb7e51ab0
SamirAllahverdi <a_nazim2007@mail.ru>
2020-08-25 13:35:33 +0400

    --- breathtaking MaxMin solution added.

--- src/main/java/hackerRank/medium/MaxMin.java
+++ src/main/java/hackerRank/medium/MaxMin.java
@@ -6,54 +6,56 @@ import java.util.Arrays;
 
 public class MaxMin {
 
-    public static void main(String[] args) {
-        int k = 2;
-        int[] arr = {1, 4, 7, 2};
+    /**
+     * https://www.hackerrank.com/challenges/angry-children/problem
+     */
 
+
+    public static void main(String[] args) {
+        int k = 4;
+        int[] arr = {1, 2, 3, 4, 10, 20, 30, 40, 100, 200};
+        // For first
         System.out.println(maxMin(k, arr));
+        // For second
+        System.out.println(maxMinOptimized(k, arr));
     }
 
-
+    /**
+     * - first solution -
+     */
     static int maxMin(int k, int[] arr) {
 
         int left = 0;
-        int right = left + k;
+        int right = left + k - 1;
+
         int unfairness = Integer.MAX_VALUE;
 
         Arrays.sort(arr);
-        System.out.println("Array " + Arrays.toString(arr));
-
-        int min = Integer.MAX_VALUE;
-        int max = Integer.MIN_VALUE;
-
-        for (int a = left; a < right; a++) {
-            System.out.println("Inside a loop " + a);
-            min = Math.min(min, arr[a]);
-            max = Math.max(max, arr[a]);
-        }
-        unfairness = Math.min(unfairness, max - min);
-
-        left++;
-        right = left + k;
-
-        while (right <= arr.length) {
-
-            System.out.println("LEFT " + left);
-            System.out.println("RIGHT " + right);
-            if (right == arr.length) {
-                max = Math.max(max, arr[right-1]);
-            }else {
-
-                max = Math.max(max, arr[right]);
-            }
-            min = Math.min(min, arr[left]);
+        while (right < arr.length) {
+            int min = arr[left];
+            int max = arr[right];
 
             unfairness = Math.min(unfairness, max - min);
-            System.out.println("Unfairness " + unfairness);
+
             left++;
-            right = left + k;
+            right = left + k - 1;
         }
 
         return unfairness;
     }
+
+    /**
+     * - second solution -
+     */
+    static int maxMinOptimized(int k, int[] arr) {
+        int unfairness = Integer.MAX_VALUE;
+        Arrays.sort(arr);
+
+        for (int i = 0; i <= arr.length - k; i++) {
+            int currUnfairness = arr[i + k - 1] - arr[i];
+            unfairness = Math.min(unfairness, currUnfairness);
+        }
+        return unfairness;
+    }
+
 }
