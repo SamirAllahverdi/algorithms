b904ec247f7a3164518bef498081d6db6144dd84
SamirAllahverdi <a_nazim2007@mail.ru>
2020-09-01 21:35:07 +0400

    --- BestTimeToBuy solution added.

---
+++ src/main/java/leetcode/easy/BestTimeToBuy.java
@@ -0,0 +1,62 @@
+package leetcode.easy;
+
+public class BestTimeToBuy {
+
+
+    /**
+     * https://leetcode.com/problems/best-time-to-buy-and-sell-stock/submissions/
+     */
+
+
+    public static void main(String[] args) {
+
+
+        int[] array = {7, 1, 5, 3, 6, 4};
+
+
+//        FOR FIRST
+        System.out.println(maxProfit(array));
+
+//        FOR SECOND
+        System.out.println(maxProfit2(array));
+    }
+
+
+    /**
+     * - first solution
+     */
+    public static int maxProfit(int[] prices) {
+
+        int idx = 0;
+        int max = 0;
+
+        while (idx < prices.length) {
+            for (int a = idx; a < prices.length; a++) {
+                if (prices[idx] < prices[a]) {
+                    max = Math.max(max, prices[a] - prices[idx]);
+                }
+            }
+            idx++;
+        }
+
+        return max;
+    }
+
+    /**
+     * - second solution
+     */
+    public static int maxProfit2(int[] prices) {
+
+        int minPrice = Integer.MAX_VALUE;
+        int maxProfit = 0;
+
+        for (int price : prices) {
+            if (price < minPrice) {
+                minPrice = price;
+            }
+            maxProfit = Math.max(maxProfit, price - minPrice);
+        }
+
+        return maxProfit;
+    }
+}
--- src/main/java/leetcode/easy/CountGoodTriplets.java
+++ src/main/java/leetcode/easy/CountGoodTriplets.java
@@ -15,23 +15,22 @@ public class CountGoodTriplets {
 
 
     public static int countGoodTriplets(int[] arr, int a, int b, int c) {
-        int n = arr.length;
 
+        int n = arr.length;
         int count = 0;
 
         for (int i = 0; i <= n - 3; i++) {
             for (int j = i+1; j <= n - 2; j++) {
-                if (Math.abs(arr[j] - arr[i]) > a) {
-                    continue;
-                }
+                if (Math.abs(arr[j] - arr[i]) > a) { continue; }
+
                 for (int k = j+1; k < n; k++) {
                     if (Math.abs(arr[i] - arr[k]) <= c && Math.abs(arr[j] - arr[k]) <= b) {
                         count++;
                     }
                 }
-
             }
         }
+
         return count;
 
     }
