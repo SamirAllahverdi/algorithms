9303b396d8e6078e21899bab6899914ad59c8731
SamirAllahverdi <a_nazim2007@mail.ru>
2020-09-04 09:54:58 +0400

    --- HowManyNumbersAreSmallerThanTheCurrentNumber added.(perfect solutions)

---
+++ src/main/java/leetcode/easy/HowManyNumbersAreSmallerThanTheCurrentNumber.java
@@ -0,0 +1,68 @@
+package leetcode.easy;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.stream.IntStream;
+
+public class HowManyNumbersAreSmallerThanTheCurrentNumber {
+
+    /**
+     * https://leetcode.com/problems/how-many-numbers-are-smaller-than-the-current-number/
+     */
+
+    public static void main(String[] args) {
+
+        int[] nums = {8, 1, 2, 2, 3};
+        System.out.println(Arrays.toString(smallerNumbersThanCurrent2(nums)));
+
+    }
+
+
+    public static int[] smallerNumbersThanCurrent(int[] nums) {
+        int[] result = new int[nums.length];
+
+        for (int a = 0; a < nums.length; a++) {
+            int count = 0;
+            for (int num : nums) {
+                if (num == nums[a]) continue;
+                if (num < nums[a]) {
+                    count++;
+                }
+            }
+            result[a] = count;
+        }
+
+        return result;
+    }
+
+    public static int[] smallerNumbersThanCurrent2(int[] nums) {
+        int max = 0;
+        for (int num : nums) {
+            max = Math.max(max, num);
+        }
+
+        int[] count = new int[max + 1];
+
+        count[0] = 0;
+
+        for (int num : nums) {
+            count[num]++;
+        }
+        for (int i = 1; i <= max; i++) {
+            count[i] += count[i - 1];
+        }
+
+        int[] res = new int[nums.length];
+
+        int idx = 0;
+
+        while (idx < res.length) {
+            if (nums[idx] == 0) res[idx] = 0;
+            else {
+                res[idx] = count[nums[idx] - 1];
+            }
+            idx++;
+        }
+        return res;
+    }
+}
