f8a897bfe9fecea676e430cca5337a413cf059a1
SamirAllahverdi <a_nazim2007@mail.ru>
2020-09-13 18:58:13 +0400

    --- amazing MinimumRemoveToMakeValidParentheses solutions added. (new level unlocked)

---
+++ src/main/java/leetcode/medium/MinimumRemoveToMakeValidParentheses.java
@@ -0,0 +1,68 @@
+package leetcode.medium;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+public class MinimumRemoveToMakeValidParentheses {
+
+    /**
+     * https://leetcode.com/problems/minimum-remove-to-make-valid-parentheses/
+     */
+
+    public static void main(String[] args) {
+
+        String s = "lee(t(c)o)de)";
+
+        System.out.println(minRemoveToMakeValid(s));
+
+    }
+
+    /**
+     * - first solution -
+     */
+    public static String minRemoveToMakeValid(String s) {
+        StringBuilder st = new StringBuilder(s);
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < st.length(); ++i) {
+            if (st.charAt(i) == '(') stack.add(i);
+            if (st.charAt(i) == ')') {
+                if (!stack.empty()) stack.pop();
+                else st.setCharAt(i, '*');
+            }
+        }
+        while (!stack.empty())
+            st.setCharAt(stack.pop(), '*');
+        return st.toString().replaceAll("\\*", "");
+    }
+
+    /**
+     * - second solution -
+     */
+    public static String minRemoveToMakeValid2(String s) {
+        boolean[] b = new boolean[s.length()];
+
+        StringBuilder st = new StringBuilder("");
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = 0; i < s.length(); ++i) {
+            if (s.charAt(i) == '(') {
+                stack.push(i);
+            } else if (s.charAt(i) == ')') {
+                if (!stack.isEmpty()) {
+                    b[i] = true;
+                    Integer pop = stack.pop();
+                    b[pop] = true;
+                }
+            } else {
+                b[i] = true;
+            }
+        }
+
+        for (int i = 0; i < s.length(); ++i) {
+            if (b[i]) st.append(s.charAt(i));
+        }
+
+        return st.toString();
+    }
+}
