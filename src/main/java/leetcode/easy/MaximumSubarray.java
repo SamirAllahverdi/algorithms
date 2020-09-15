c0654f2b6643981cf5acdb6b7759d0b8c6729f21
SamirAllahverdi <a_nazim2007@mail.ru>
2020-09-15 12:07:15 +0400

    --- crazy solution added. (MaximumSubarray)

---
+++ src/main/java/leetcode/easy/MaximumSubarray.java
@@ -0,0 +1,24 @@
+package leetcode.easy;
+
+public class MaximumSubarray {
+
+    /**
+     * https://leetcode.com/problems/maximum-subarray/
+     */
+
+    public static void main(String[] args) {
+        int[] nums = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+        System.out.println(maxSubArray(nums));
+    }
+
+
+    public static int maxSubArray(int [] A) {
+        int maxSoFar=A[0], maxEndingHere=A[0];
+        for (int i=1;i<A.length;++i){
+            maxEndingHere= Math.max(maxEndingHere+A[i],A[i]);
+            maxSoFar=Math.max(maxSoFar, maxEndingHere);
+        }
+        return maxSoFar;
+    }
+
+}
--- src/main/java/leetcode/medium/MinimumRemoveToMakeValidParentheses.java
+++ src/main/java/leetcode/medium/MinimumRemoveToMakeValidParentheses.java
@@ -1,7 +1,5 @@
 package leetcode.medium;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Stack;
 
 public class MinimumRemoveToMakeValidParentheses {
