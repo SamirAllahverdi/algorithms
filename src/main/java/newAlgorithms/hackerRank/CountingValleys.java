95beaf6fc5495d2c6eda9d4bc64e3bf9cba02a5f
Samir Allahverdi <samir.a@crocusoft.com>
2021-05-24 23:42:03 +0400

    --- CountingValleys

---
+++ src/main/java/newAlgorithms/hackerRank/CountingValleys.java
@@ -0,0 +1,37 @@
+package newAlgorithms.hackerRank;
+
+import java.util.Arrays;
+
+public class CountingValleys {
+
+    /**
+     * https://www.hackerrank.com/challenges/counting-valleys
+     */
+
+    public static void main(String[] args) {
+        System.out.println(countingValleys(8, "UDDDUDUU"));  //expected 1
+        System.out.println(countingValleys(12,"DDUUDDUDUUUD")); //expected 2
+    }
+
+    public static int countingValleys(int steps, String path) {
+
+        int current = 0;
+        int countValley = 0;
+
+        for (int a = 0; a < steps; a++) {
+
+            if (path.charAt(a) == 'U') {
+                ++current;
+            } else {
+                --current;
+                if (current == -1) {
+                    ++countValley;
+                }
+            }
+        }
+
+        return countValley;
+    }
+
+}
+
