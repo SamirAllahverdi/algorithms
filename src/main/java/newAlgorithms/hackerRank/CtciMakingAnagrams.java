d3b9e1607b547b403f35214c585a1fa4c6fb413d
Samir Allahverdi <samir.a@crocusoft.com>
2021-06-16 08:07:10 +0400

    --- CtciMakingAnagrams

--- src/main/java/newAlgorithms/hackerRank/CtciMakingAnagrams.java
+++ src/main/java/newAlgorithms/hackerRank/CtciMakingAnagrams.java
@@ -11,25 +11,24 @@ public class CtciMakingAnagrams {
     public static final String SECOND = "second_";
 
 
-//    public static void main(String[] args) {
-//        System.out.println(makeAnagram("fcrxzwscanmligyxyvym", "jxwtrhvujlmrpdoqbisbwhmgpmeoke"));
-////        expected 30
-//    }
+    public static void main(String[] args) {
+        System.out.println(makeAnagram("fcrxzwscanmligyxyvym", "jxwtrhvujlmrpdoqbisbwhmgpmeoke"));
+//        expected 30
+    }
 
     public static int makeAnagram(String first, String second) {
 
         Map<String, Integer> map = new HashMap<>();
 
-
         first.chars().forEach(character -> {
-                    final String firstWordKey = String.format("%s%s", FIRST, Character.getName(character));
+                    final String firstWordKey = String.format("%s%s", FIRST, Character.toString(character));
                     map.put(firstWordKey, map.getOrDefault(firstWordKey, 0) + 1);
                 }
         );
 
         second.chars().forEach(character -> {
-                    final String firstWordKey = String.format("%s%s", FIRST, Character.getName(character));
-                    final String secondWordKey = String.format("%s%s", SECOND, Character.getName(character));
+                    final String firstWordKey = String.format("%s%s", FIRST, Character.toString(character));
+                    final String secondWordKey = String.format("%s%s", SECOND, Character.toString(character));
 
                     if (map.containsKey(firstWordKey) && map.get(firstWordKey) > 0) {
                         map.put(firstWordKey, map.get(firstWordKey) - 1);
