a7c3838eab94560767e2d9cd58bac6fe9763396c
Samir Allahverdi <samir.a@crocusoft.com>
2021-06-26 10:07:00 +0400

    --- Kangaroo

---
+++ src/main/java/newAlgorithms/hackerRank/Kangaroo.java
@@ -0,0 +1,16 @@
+package newAlgorithms.hackerRank;
+
+public class Kangaroo {
+
+    /**
+     * https://www.hackerrank.com/challenges/kangaroo
+     */
+//    public static void main(String[] args) {
+//        System.out.println(kangaroo(0, 2, 5, 3)); /* expected NO */
+//    }
+
+    public static String kangaroo(int x1, int v1, int x2, int v2) {
+        return v2 - v1 != 0 && (x1 - x2) / (v2 - v1) > 0 && (x1 - x2) % (v2 - v1) == 0
+                ? "YES" : "NO";
+    }
+}
