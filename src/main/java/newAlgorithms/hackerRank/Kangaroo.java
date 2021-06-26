f0644921163996bc297a5f820703e278a66abd1c
Samir Allahverdi <samir.a@crocusoft.com>
2021-06-26 10:19:43 +0400

    --- Kangaroo

--- src/main/java/newAlgorithms/hackerRank/Kangaroo.java
+++ src/main/java/newAlgorithms/hackerRank/Kangaroo.java
@@ -2,13 +2,13 @@ package newAlgorithms.hackerRank;
 
 public class Kangaroo {
 
-    /**
-     * https://www.hackerrank.com/challenges/kangaroo
-     */
-    public static void main(String[] args) {
-        System.out.println(kangaroo(0, 2, 5, 3)); /* expected NO */
-        System.out.println(kangaroo(0, 3, 4, 2));  /* expected YES */
-    }
+//    /**
+//     * https://www.hackerrank.com/challenges/kangaroo
+//     */
+//    public static void main(String[] args) {
+//        System.out.println(kangaroo(0, 2, 5, 3)); /* expected NO */
+//        System.out.println(kangaroo(0, 3, 4, 2));  /* expected YES */
+//    }
 
     public static String kangaroo(int x1, int v1, int x2, int v2) {
         return v1 > v2  &&  (x1 - x2) % (v2 - v1) == 0
