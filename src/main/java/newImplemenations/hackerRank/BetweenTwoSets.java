f8b56536fc3b6bcd9e368e6de087fce5cdd8e686
Samir Allahverdi <samir.a@crocusoft.com>
2021-12-16 23:15:52 +0400

    --- BetweenTwoSets

---
+++ src/main/java/newImplemenations/hackerRank/BetweenTwoSets.java
@@ -0,0 +1,36 @@
+package newImplemenations.hackerRank;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.IntStream;
+
+public class BetweenTwoSets {
+
+
+    /**
+     * https://www.hackerrank.com/challenges/between-two-sets
+     */
+//    public static void main(String[] args) {
+//        System.out.println(getTotalX(Arrays.asList(2, 6), Arrays.asList(24, 36)));
+//    }
+
+    public static int getTotalX(List<Integer> a, List<Integer> b) {
+
+        Collections.sort(a);
+        Collections.sort(b);
+        int min = a.get(0);
+        int max = b.get(b.size() - 1);
+
+        return (int) IntStream.rangeClosed(min, max).filter(i -> hasFactors(i, a) && isFactor(i, b)).count();
+    }
+
+
+    public static boolean hasFactors(int num, List<Integer> list) {
+        return list.stream().allMatch(a -> num % a == 0);
+    }
+
+    public static boolean isFactor(int num, List<Integer> list) {
+        return list.stream().allMatch(a -> a % num == 0);
+    }
+}
