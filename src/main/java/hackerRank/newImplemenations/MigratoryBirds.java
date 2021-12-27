cfa09f4616893af769984b71b0e92d4be9edaff7
Samir Allahverdi <samir.a@crocusoft.com>
2021-12-27 23:58:36 +0400

    --- MigratoryBirds

---
+++ src/main/java/hackerRank/newImplemenations/MigratoryBirds.java
@@ -0,0 +1,37 @@
+package hackerRank.newImplemenations;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class MigratoryBirds {
+
+    /**
+     * https://www.hackerrank.com/challenges/migratory-birds
+     */
+
+//    public static void main(String[] args) {
+//        System.out.println(migratoryBirds(Arrays.asList(1, 2, 3, 4, 5, 4, 3, 2, 1, 3, 4)));
+//    }
+
+    public static int migratoryBirds(List<Integer> arr) {
+        int[] counter = new int[6];
+        int max = Integer.MIN_VALUE;
+        int result = Integer.MIN_VALUE;
+
+        for (int a : arr) {
+            counter[a]++;
+            if (counter[a] > max) {
+                max = counter[a];
+                result = a;
+            } else if (counter[a] == max) {
+                result = Math.min(result, a);
+            }
+        }
+
+        return result;
+    }
+
+
+}
