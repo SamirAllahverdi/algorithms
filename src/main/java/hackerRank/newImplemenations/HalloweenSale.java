ac25ce2b3729a2dd14a1e0e158fec17879d50bc5
Samir Allahverdi <samir.a@crocusoft.com>
2022-01-06 23:42:45 +0400

    --- HalloweenSale

---
+++ src/main/java/hackerRank/newImplemenations/HalloweenSale.java
@@ -0,0 +1,22 @@
+package hackerRank.newImplemenations;
+
+public class HalloweenSale {
+
+    /**
+     * https://www.hackerrank.com/challenges/halloween-sale
+     */
+//    public static void main(String[] args) {
+//        System.out.println(howManyGames(20, 3, 6, 80)); //expected 6
+//    }
+
+
+    public static int howManyGames(int p, int d, int m, int s) {
+        int count = 0;
+        while (s >= p) {
+            count++;
+            s -= p;
+            p = Math.max(p - d, m);
+        }
+        return count;
+    }
+}
