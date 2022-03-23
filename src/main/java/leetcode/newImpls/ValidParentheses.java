c14bff31a77f3246f5400ab5d645c716131b3ced
Samir Allahverdi <a_nazim2007@mail.ru>
2022-03-23 18:07:49 +0400

    ValidParentheses

---
+++ src/main/java/leetcode/newImpls/ValidParentheses.java
@@ -0,0 +1,34 @@
+package leetcode.newImpls;
+
+import java.util.*;
+
+public class ValidParentheses {
+
+    /**
+     * https://leetcode.com/problems/valid-parentheses/
+     */
+//    public static void main(String[] args) {
+//
+//        System.out.println(isValid("))"));
+//        System.out.println(isValid("()[]{}"));
+//    }
+//
+
+    public static boolean isValid(String s) {
+        if (s == null || s.length() % 2 != 0) return false;
+        Stack<Character> stack = new Stack<>();
+
+        for (char currChar : s.toCharArray()) {
+            if (currChar == '(')
+                stack.push(')');
+            else if (currChar == '{')
+                stack.push('}');
+            else if (currChar == '[')
+                stack.push(']');
+            else if (stack.isEmpty() || stack.pop() != currChar)
+                return false;
+        }
+
+        return stack.isEmpty();
+    }
+}
