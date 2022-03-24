e3d2d442487d19d2877af996f05e4648f4a7dc6a
Samir Allahverdi <a_nazim2007@mail.ru>
2022-03-24 16:11:53 +0400

    MergeTwoSortedList & RemoveDuplicatesFromSortedArray

---
+++ src/main/java/leetcode/newImpls/MergeTwoSortedList.java
@@ -0,0 +1,49 @@
+package leetcode.newImpls;
+
+public class MergeTwoSortedList {
+    static class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {
+        }
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        @Override
+        public String toString() {
+            return "ListNode{" +
+                    "val=" + val +
+                    ", next=" + next +
+                    '}';
+        }
+    }
+
+    /**
+     * https://leetcode.com/problems/merge-two-sorted-lists/submissions/
+     */
+//    public static void main(String[] args) {
+//
+//        ListNode l1 = new ListNode();
+//        ListNode l2 = new ListNode();
+//        l2.next = new ListNode(2);
+//
+//        System.out.println(mergeTwoLists(l1, l2));
+//    }
+
+
+    public static ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null) return l2;
+        if (l2 == null) return l1;
+
+        if (l1.val < l2.val) {
+            l1.next = mergeTwoLists(l1.next, l2);
+            return l1;
+        } else {
+            l2.next = mergeTwoLists(l1, l2.next);
+            return l2;
+        }
+    }
+
+
+}
---
+++ src/main/java/leetcode/newImpls/RemoveDuplicatesFromSortedArray.java
@@ -0,0 +1,31 @@
+package leetcode.newImpls;
+
+public class RemoveDuplicatesFromSortedArray {
+
+
+    /***
+     *  https://leetcode.com/problems/remove-duplicates-from-sorted-array/
+     */
+
+//    public static void main(String[] args) {
+//        int[] nums = {0, 0, 1, 1, 1, 2, 2, 3, 3, 4};
+//        System.out.println(removeDuplicates(nums));
+//    }
+
+
+    public static int removeDuplicates(int[] nums) {
+        if (nums.length == 0) return 0;
+
+        int i = 1;
+        for (int n : nums) {
+            if (n > nums[i - 1]) {
+                nums[i] = n;
+                i++;
+            }
+        }
+
+        return i;
+    }
+
+
+}
