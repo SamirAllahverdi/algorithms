3bb977b442af2c6273165d50ad979666edaa777d
Samir Allahverdi <a_nazim2007@mail.ru>
2022-03-27 23:28:29 +0400

    MaximumSubArray

---
+++ src/main/java/leetcode/newImpls/MaximumSubArray.java
@@ -0,0 +1,31 @@
+package leetcode.newImpls;
+
+public class MaximumSubArray {
+    /**
+     * https://leetcode.com/problems/maximum-subarray/
+     */
+//    public static void main(String[] args) {
+//        int[] nums = {5, 4, -1, 7, 8};
+//        System.out.println(maxSubArray(nums));
+//    }
+
+    public static int maxSubArray(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+
+        int max = nums[0], sum = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            if (sum < 0) {
+                sum = nums[i];
+            } else { //sum >= 0
+                sum += nums[i];
+            }
+            max = Math.max(max, sum);
+        }
+
+
+        return max;
+    }
+
+}
