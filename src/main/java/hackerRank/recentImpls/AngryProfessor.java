48b2e2ef0ec6eb25f41be26e33697abe1182be5f
Samir Allahverdi <a_nazim2007@mail.ru>
2022-06-09 08:52:48 +0400

    AngryProfessor

---
+++ src/main/java/hackerRank/recentImpls/AngryProfessor.java
@@ -0,0 +1,21 @@
+package hackerRank.recentImpls;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class AngryProfessor {
+
+    public static void main(String[] args) {
+        List<Integer> integers = Arrays.asList(-2, -1, 0, 1, 2);
+        System.out.println(angryProfessor(3, integers));
+    }
+
+    public static String angryProfessor(int k, List<Integer> a) {
+
+        for (int b : a)
+            if (b <= 0)
+                k--;
+
+        return k > 0 ? "YES" : "NO";
+    }
+}
