c44d6792a2d8dfa81cff1cfcb80ab998d25c8b86
Samir Allahverdi <a_nazim2007@mail.ru>
2022-06-10 08:57:11 +0400

    BirthdayCakeCandle

--- src/main/java/hackerRank/recentImpls/AngryProfessor.java
+++ src/main/java/hackerRank/recentImpls/AngryProfessor.java
@@ -5,10 +5,10 @@ import java.util.List;
 
 public class AngryProfessor {
 
-    public static void main(String[] args) {
-        List<Integer> integers = Arrays.asList(-2, -1, 0, 1, 2);
-        System.out.println(angryProfessor(3, integers));
-    }
+//    public static void main(String[] args) {
+//        List<Integer> integers = Arrays.asList(-2, -1, 0, 1, 2);
+//        System.out.println(angryProfessor(3, integers));
+//    }
 
     public static String angryProfessor(int k, List<Integer> a) {
 
