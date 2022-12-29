e2200b5b79245d10184ef692a6566e97099eec0e
Samir_Allahverdiyev <smrallahverdieff@gmail.com>
2022-12-29 12:04:11 +0100

    ClimbingStairs

---
+++ src/main/java/leetcode/easy/ClimbingStairs.java
@@ -0,0 +1,34 @@
+package leetcode.easy;
+
+public class ClimbingStairs {
+
+    /**
+     * - https://leetcode.com/problems/climbing-stairs
+     */
+//    public static void main(String[] args) {
+//        System.out.println(climbStairs(10));
+//    }
+
+    public static int climbStairs(int n) {
+        if (n == 1) {
+            return 1;
+        }
+        if (n == 2) {
+            return 2;
+        }
+
+        int[] dp = prepare(n);
+
+        for (int i = 3; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[n];
+    }
+
+    private static int[] prepare(int n) {
+        int[] dp = new int[n + 1];
+        dp[1] = 1;
+        dp[2] = 2;
+        return dp;
+    }
+}
